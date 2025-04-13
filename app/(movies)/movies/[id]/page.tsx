c574034f046171c58e8b52cceb60c3ac4d42e9e5
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-14 01:58:53 +0900

    params error fix

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,8 +2,17 @@ import {Suspense} from "react";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-const MovieDetail = async ({params}: {params: {id: string}}) => {
-  const {id} = await params;
+interface IParameters {
+  params: {id: string};
+}
+
+export async function generateMetadata({params: {id}}: IParameters) {
+  return {
+    title: "",
+  };
+}
+
+const MovieDetail = async ({params: {id}}: IParameters) => {
   return (
     <>
       <Suspense fallback={<h1>Loading Movie Info</h1>}>
