ac8e6a9c34dde81c86b82068fd8cec76c7467bb3
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-14 02:27:42 +0900

    prefetch

--- components/movie-card.tsx
+++ components/movie-card.tsx
@@ -18,7 +18,9 @@ const MovieCard = ({poster_path, title, id}: IMovieCardProps) => {
   return (
     <li className={styles.movie}>
       <img src={poster_path} alt={title} onClick={onClick} />
-      <Link href={`/movies/${id}`}>{title}</Link>
+      <Link prefetch href={`/movies/${id}`}>
+        {title}
+      </Link>
     </li>
   );
 };
