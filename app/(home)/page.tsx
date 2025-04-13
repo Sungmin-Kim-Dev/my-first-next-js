ab13796859e156c43a13f75641159594eba30957
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-13 22:20:45 +0900

    fetch in next js way

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,30 +1,34 @@
-"use client";
+import Link from "next/link";
 
-import {useEffect, useState} from "react";
+export const metadata = {
+  title: "Home",
+};
 
-// export const metadata = {
-//   title: "Home",
-// };
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-const page = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
+async function getMovies() {
+  console.log("fetching...");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // return fetch(URL).then((response) => response.json());
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
 
+const Homepage = async () => {
+  const movies = await getMovies();
   return (
     <div>
       <h1>Hello, Next.js!</h1>
-      {isLoading ? <h2>Loading...</h2> : `${JSON.stringify(movies)}`}
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default page;
+export default Homepage;
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,11 +2,9 @@
 
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {useState} from "react";
 
 const Navigation = () => {
   const path = usePathname();
-  const [count, setCount] = useState(0);
 
   return (
     <nav>
@@ -18,9 +16,6 @@ const Navigation = () => {
         <li>
           <Link href="/about-us">About Us</Link>
           {path === "/about-us" && "💕"}
-        </li>
-        <li>
-          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
