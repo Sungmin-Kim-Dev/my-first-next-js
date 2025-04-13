e37a79547e1fb8b9878f3aa2ccebdf4295fc3e7b
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-04-14 02:13:59 +0900

    fix params

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,18 +1,26 @@
 import {Suspense} from "react";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+import {API_URL} from "../../../../components/API_URL";
 
-interface IParameters {
-  params: {id: string};
-}
+type IParams = Promise<{
+  id: string;
+}>;
 
-export async function generateMetadata({params: {id}}: IParameters) {
+export async function generateMetadata(props: {params: IParams}) {
+  const params = await props.params;
+  const id = params.id;
+  const response = await fetch(`${API_URL}/${id}`);
+  const json = await response.json();
+  const movie = json;
   return {
-    title: "",
+    title: movie.title,
   };
 }
 
-const MovieDetail = async ({params: {id}}: IParameters) => {
+const MovieDetail = async (props: {params: IParams}) => {
+  const params = await props.params;
+  const id = params.id;
   return (
     <>
       <Suspense fallback={<h1>Loading Movie Info</h1>}>
